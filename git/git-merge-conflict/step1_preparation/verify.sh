b9145b2892199c676226ff4dfddf8553bd746b54
Taylan Oezer <s85500@htw-dresden.de>
2024-10-05 09:31:45 +0200

    'git-merge-conflict:' adjusting text and adding verify.sh to 'step1_preparation'

---
+++ git/git-merge-conflict/step1_preparation/verify.sh
@@ -0,0 +1,19 @@
+# Prüfen, ob der Befehl ausgeführt wurde
+if ! history | grep -q "mkdir git-merge-conflict-demo"; then
+    echo "Error: 'mkdir git-merge-conflict-demo' has not been executed."
+    exit 1
+fi
+
+
+if ! history | grep -q "cd git-merge-conflict-demo"; then
+    echo "Error: 'cd git-merge-conflict-demo' has not been executed."
+    exit 1
+fi
+
+if ! history | grep -q "git init"; then
+    echo "Error: 'git init' has not been executed."
+    exit 1
+fi
+
+
+exit 0
