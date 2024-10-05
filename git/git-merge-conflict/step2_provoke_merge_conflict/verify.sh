7bd58b4d1364f41604e9236f08c7109e5c0bf6e6
Taylan Oezer <s85500@htw-dresden.de>
2024-10-05 10:24:05 +0200

    adjusting text in 'step2_provoke_merge_conflict' and adding 'verify.sh'

---
+++ git/git-merge-conflict/step2_provoke_merge_conflict/verify.sh
@@ -0,0 +1,28 @@
+# Prüfen, ob der Befehl ausgeführt wurde
+if ! history | grep -q "git switch main"; then
+    echo "Error: 'git switch main' has not been executed."
+    exit 1
+fi
+
+
+if ! history | grep -q "echo "Hello from the main branch again" > file.txt"; then
+    echo "Error: 'echo "Hello from the main branch again" > file.txt' has not been executed."
+    exit 1
+fi
+
+if ! history | grep -q "git add file.txt"; then
+    echo "Error: 'git add file.txt' has not been executed."
+    exit 1
+fi
+
+if ! history | grep -q "git commit -m "Modify file.txt on main""; then
+    echo "Error: 'git commit -m "Modify file.txt on main"' has not been executed."
+    exit 1
+fi
+
+if ! history | grep -q "git merge my-first-branch2"; then
+    echo "Error: 'git merge my-first-branch2' has not been executed."
+    exit 1
+fi
+
+exit 0
