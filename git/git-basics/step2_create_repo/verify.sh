5eda9eabc0f12feada5bccdbb0879c94a536b7a1
Taylan Oezer <s85500@htw-dresden.de>
2024-10-12 21:29:55 +0200

    git_course -> adjusting text + verify.sh in 'step2_create_repo'

--- git/git-basics/step2_create_repo/verify.sh
+++ git/git-basics/step2_create_repo/verify.sh
@@ -1,20 +1,19 @@
 # Prüfen, ob der Befehl ausgeführt wurde
-if ! history | grep -q "mkdir my-first-git-project"; then
-    echo "Error: 'mkdir my-first-git-project' has not been executed."
-    exit 1
-fi
+#if ! history | grep -q "mkdir my-first-git-project"; then
+#    echo "Error: 'mkdir my-first-git-project' has not been executed."
+#    exit 1
+#fi
 
-if ! history | grep -q "cd my-first-git-project"; then
-    echo "Error: 'cd my-first-git-project' has not been executed."
-    exit 1
-fi
+#if ! history | grep -q "cd my-first-git-project"; then
+#    echo "Error: 'cd my-first-git-project' has not been executed."
+#    exit 1
+#fi
 
 
-if ! history | grep -q "git init"; then
-    echo "Error: 'git init' has not been executed."
-    exit 1
-fi
-
+#if ! history | grep -q "git init"; then
+#    echo "Error: 'git init' has not been executed."
+#    exit 1
+#fi
 
 
 if ! history | grep -q "git status"; then
