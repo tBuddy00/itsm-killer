1ab023bb88481a2b4263d46355227c22c8de22cc
Taylan Oezer <s85500@htw-dresden.de>
2024-10-12 22:22:04 +0200

    git_course -> adjusting text + verify.sh in 'step1_preparation'

--- git/git-merge-conflict/step1_preparation/verify.sh
+++ git/git-merge-conflict/step1_preparation/verify.sh
@@ -5,20 +5,20 @@ if ! history | grep -q "mkdir git-merge-conflict-demo"; then
 fi
 
 
-if ! history | grep -q "cd git-merge-conflict-demo"; then
-    echo "Error: 'cd git-merge-conflict-demo' has not been executed."
-    exit 1
-fi
+#if ! history | grep -q "cd git-merge-conflict-demo"; then
+#    echo "Error: 'cd git-merge-conflict-demo' has not been executed."
+#    exit 1
+#fi
 
-if ! history | grep -q "git init"; then
-    echo "Error: 'git init' has not been executed."
-    exit 1
-fi
+#if ! history | grep -q "git init"; then
+#    echo "Error: 'git init' has not been executed."
+#    exit 1
+#fi
 
-if ! history | grep -q "git switch -c my-first-branch2"; then
-    echo "Error: 'git switch -c my-first-branch2' has not been executed."
-    exit 1
-fi
+#if ! history | grep -q "git switch -c my-first-branch2"; then
+#    echo "Error: 'git switch -c my-first-branch2' has not been executed."
+#    exit 1
+#fi
 
 
 exit 0
