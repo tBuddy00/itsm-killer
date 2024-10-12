8ec41a5b16e5163ddfa481a081476e60a5b95763
Taylan Oezer <s85500@htw-dresden.de>
2024-10-12 21:58:34 +0200

    git_course -> adjusting text + verify.sh in 'step5_making_changes_on_branch

--- git/git-basics/step5_making_changes_on_branch/verify.sh
+++ git/git-basics/step5_making_changes_on_branch/verify.sh
@@ -1,18 +1,31 @@
-# Prüfen, ob der Befehl ausgeführt wurde
-if ! history | grep -q "echo "This is a new feature." >> hello.txt"; then
-    echo "echo "This is a new feature." >> hello.txt' has not been executed."
+# Prüfen, ob 'hello.txt' existiert
+if [ ! -f hello.txt ]; then
+    echo "Error: File 'hello.txt' does not exist."
     exit 1
 fi
 
-if ! history | grep -q "git add hello.txt"; then
-    echo "Error: 'git add hello.txt' has not been executed."
+# Überprüfen, ob der Text 'This is a new feature.' in der Datei enthalten ist
+if ! grep -q "This is a new feature." hello.txt; then
+    echo "Error: The content 'This is a new feature.' was not found in 'hello.txt'."
     exit 1
 fi
 
 
-if ! history | grep -q "git commit -m "adding a new feature to 'hello.txt'""; then
-    echo "Error: 'git commit -m "adding a new feature to 'hello.txt'"' has not been executed."
-    exit 1
-fi
+
+#if ! history | grep -q "echo "This is a new feature." >> hello.txt"; then
+#    echo "echo "This is a new feature." >> hello.txt' has not been executed."
+#    exit 1
+#fi
+
+#if ! history | grep -q "git add hello.txt"; then
+#    echo "Error: 'git add hello.txt' has not been executed."
+#    exit 1
+#fi
+
+
+#if ! history | grep -q "git commit -m "adding a new feature to 'hello.txt'""; then
+#    echo "Error: 'git commit -m "adding a new feature to 'hello.txt'"' has not been executed."
+#    exit 1
+#fi
 
 exit 0
