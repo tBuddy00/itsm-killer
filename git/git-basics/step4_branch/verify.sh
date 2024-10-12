d557c58c056896f7bb0a04d10b4d6eff8b5cf47c
Taylan Oezer <s85500@htw-dresden.de>
2024-10-12 21:52:23 +0200

    git_course -> adjusting text + verify.sh in 'step4_branch

--- git/git-basics/step4_branch/verify.sh
+++ git/git-basics/step4_branch/verify.sh
@@ -1,6 +1,6 @@
-# Prüfen, ob der Befehl ausgeführt wurde
-if ! history | grep -q "git branch my-first-branch"; then
-    echo "Error: 'git branch my-first-branch' has not been executed."
+# Prüft, ob 'my-first-branch' existiert als Branch
+if ! git branch --list | grep -q "my-first-branch"; then
+    echo "Error: The branch 'my-first-branch' does not exist."
     exit 1
 fi
 
@@ -10,9 +10,9 @@ if ! history | grep -q "git branch"; then
 fi
 
 
-if ! history | grep -q "git switch my-first-branch"; then
-    echo "Error: 'git switch my-first-branch' has not been executed."
-    exit 1
-fi
+#if ! history | grep -q "git switch my-first-branch"; then
+#    echo "Error: 'git switch my-first-branch' has not been executed."
+#    exit 1
+#fi
 
 exit 0
